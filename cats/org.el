68c39db177041bacc333c0527fd4965f2f2d23e0
Misaka <chuxubank@qq.com>
2021-07-07 17:50:48 +0800

    Add org config

--- cats/org.el
+++ cats/org.el
@@ -1,1 +1,47 @@
-(setq org-startup-indented t)
+;;; startup
+(setq org-startup-indented t
+      org-link-abbrev-alist
+      '("wiki-zh" . "https://zh.wikipedia.org/wiki/%h")
+      '("wiki-en" . "https://en.wikipedia.org/wiki/%s"))
+
+;;; ui
+(with-no-warnings
+  (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
+  (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
+  (custom-declare-face '+org-todo-onhold  '((t (:inherit (bold warning org-todo)))) "")
+  (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) ""))
+(setq org-todo-keywords
+      '((sequence
+         "TODO(t)"  ; A task that needs doing & is ready to do
+         "PROJ(p)"  ; A project, which usually contains other tasks
+         "LOOP(r)"  ; A recurring task
+         "STRT(s)"  ; A task that is in progress
+         "WAIT(w)"  ; Something external is holding up this task
+         "HOLD(h)"  ; This task is paused/on hold because of me
+         "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+         "|"
+         "DONE(d)"  ; Task successfully completed
+         "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+        (sequence
+         "[ ](T)"   ; A task that needs doing
+         "[-](S)"   ; Task is in progress
+         "[?](W)"   ; Task is being held up or paused
+         "|"
+         "[X](D)")  ; Task was completed
+        (sequence
+         "|"
+         "OKAY(o)"
+         "YES(y)"
+         "NO(n)"))
+      org-todo-keyword-faces
+      '(("[-]"  . +org-todo-active)
+        ("STRT" . +org-todo-active)
+        ("[?]"  . +org-todo-onhold)
+        ("WAIT" . +org-todo-onhold)
+        ("HOLD" . +org-todo-onhold)
+        ("PROJ" . +org-todo-project)
+        ("NO"   . +org-todo-cancel)
+        ("KILL" . +org-todo-cancel)))
+
+(with-eval-after-load 'org
+  (setq org-agenda-files (list org-directory)))
