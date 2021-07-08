25ddcf556d4c9e13c071e5eb533f5d4b0d2c5193
Misaka <chuxubank@qq.com>
2021-07-08 14:07:30 +0800

    Fix pdf-view-mode

--- cats/pdf.el
+++ cats/pdf.el
@@ -1,2 +1,21 @@
 (use-package pdf-tools
-  :defer t)
+  :magic ("%PDF" . pdf-view-mode)
+  :config
+  (pdf-tools-install)
+  (defun nano-modeline-pdf-view-mode ()
+    (let ((buffer-name (format-mode-line "%b"))
+	  (mode-name   (nano-mode-name))
+	  (branch      (vc-branch))
+	  (page-number (concat
+			(number-to-string (pdf-view-current-page)) "/"
+			(or (ignore-errors
+			      (number-to-string (pdf-cache-number-of-pages)))
+			    "???"))))
+      (nano-modeline-compose
+       "RO"
+       buffer-name
+       (concat "("
+	       mode-name
+	       (if branch (concat ", " (propertize branch 'face 'italic)))
+	       ")" )
+       page-number))))
