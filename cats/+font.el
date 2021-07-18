7184ae0a3930daabe790ba4de86773054106a9e2
Misaka <chuxubank@qq.com>
2021-07-18 23:04:31 +0800

    Specify font set

--- cats/+font.el
+++ cats/+font.el
@@ -1,2 +1,7 @@
-(set-face-attribute 'default nil :font "RobotoMono NF 16")
+(set-face-attribute 'default nil :font "Roboto Mono 16")
+
+(set-fontset-font t 'symbol (font-spec :family "Segoe UI Symbol"))
+
 (set-fontset-font t 'han (font-spec :family "DengXian"))
+
+(set-fontset-font t 'unicode (font-spec :family "Segoe UI Emoji") nil 'append)
