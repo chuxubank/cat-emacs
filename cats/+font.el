623a788bc0013258daad2cba732515d6660b7438
Misaka <chuxubank@qq.com>
2021-07-19 10:59:57 +0800

    Add font config for linux

--- cats/+font.el
+++ cats/+font.el
@@ -1,7 +1,31 @@
-(set-face-attribute 'default nil :font "Roboto Mono 16")
+(defvar cat-default-font "Roboto Mono 16")
+(defvar cat-symbol-font nil)
+(defvar cat-cjk-font nil)
+(defvar cat-emoji-font nil)
 
-(set-fontset-font t 'symbol (font-spec :family "Segoe UI Symbol"))
+(when IS-LINUX
+  (setq
+   cat-cjk-font "Noto Sans CJK SC"
+   cat-emoji-font "Noto Color Emoji"))
 
-(set-fontset-font t 'han (font-spec :family "DengXian"))
+(when IS-WINDOWS
+  (setq
+   cat-symbol-font "Segoe UI Symbol"
+   cat-cjk-font "DengXian"
+   cat-emoji-font "Segoe UI Emoji"))
 
-(set-fontset-font t 'unicode (font-spec :family "Segoe UI Emoji") nil 'append)
+(set-face-attribute 'default nil :font cat-default-font :weight 'light)
+
+(when cat-symbol-font
+  (set-fontset-font t 'symbol (font-spec :family cat-symbol-font))
+  (message "Set Symbol font."))
+
+;; 猫
+(when cat-cjk-font
+  (set-fontset-font t 'han (font-spec :family cat-cjk-font))
+  (message "Set CJK font."))
+
+;; 😺
+(when cat-emoji-font
+  (set-fontset-font t 'unicode (font-spec :family cat-emoji-font) nil 'append)
+  (message "Set Emoji font."))
