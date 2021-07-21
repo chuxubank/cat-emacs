9300de27c16d5a2f71bcec6034a8104121a307f8
Misaka <chuxubank@qq.com>
2021-07-21 10:41:17 +0800

    Add nongnu to package archives

--- cats/+package.el
+++ cats/+package.el
@@ -1,5 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (setq package-check-signature nil)
 
 (require 'use-package-ensure)
