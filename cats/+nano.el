bab7b925e35efa8ae2c569da2af93a7bae5b5898
Misaka <chuxubank@qq.com>
2021-07-26 17:20:25 +0800

    Config start up theme based on system theme

--- cats/+nano.el
+++ cats/+nano.el
@@ -1,5 +1,8 @@
 (use-package nano-theme
   :straight (nano-theme :type git :host github :repo "rougier/nano-theme")
+  :init
+  (defun is-dark-mode()
+    (string-equal "0" (string-trim (shell-command-to-string "powershell -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
   :config
   (setq
    default-frame-alist (append (list '(internal-border-width . 24)
@@ -12,7 +15,13 @@
 			  'truncation (make-glyph-code ?… 'nano-faded))
   (set-display-table-slot standard-display-table
 			  'wrap (make-glyph-code ?… 'nano-faded))
-  (nano-dark))
+
+  (if IS-WINDOWS
+      (add-hook 'after-init-hook (lambda ()
+				   (if (is-dark-mode)
+				       (nano-dark)
+				     (nano-light))))
+    (nano-dark)))
 
 (use-package nano-modeline
   :straight (nano-modeline :type git :host github :repo "rougier/nano-modeline")
