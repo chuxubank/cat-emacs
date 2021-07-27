febdcde287fde0c3ad07ccf220a87ebbf6ab0d0e
Misaka <chuxubank@qq.com>
2021-07-27 22:31:27 +0800

    Windows Emacs do support svg display
    Only if librsvg is installed

--- cats/+telega.el
+++ cats/+telega.el
@@ -4,14 +4,7 @@
 (setq telega-symbol-folder "📁"
       telega-symbol-reply "↫")
 
-(when IS-WINDOWS
-  (setq telega-chat-show-avatars nil
-	telega-company-username-show-avatars nil
-	telega-root-show-avatars nil
-	telega-user-show-avatars nil))
-
 (when (featurep 'selectrum)
   (setq telega-completing-read-function #'selectrum-completing-read))
-
 
 (define-key global-map (kbd "C-c a t") telega-prefix-map)
