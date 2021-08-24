0d1ddd62efce9f48948e2a76df536101b6188c21
chuxubank <chuxubank@qq.com>
2021-08-24 18:02:55 +0800

    Fix telega font

--- cats/+telega.el
+++ cats/+telega.el
@@ -8,3 +8,8 @@
   (setq telega-completing-read-function #'selectrum-completing-read))
 
 (define-key global-map (kbd "C-c a t") telega-prefix-map)
+
+(with-eval-after-load 'telega
+  (set-face-attribute 'telega-entity-type-pre nil :family "monospace")
+  (set-face-attribute 'telega-entity-type-code nil :family "monospace")
+  (set-face-attribute 'telega-webpage-fixed nil :family "monospace"))
