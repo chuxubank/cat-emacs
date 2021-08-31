002a44d12a08f106f9af6292bef7452adc577909
chuxubank <chuxubank@qq.com>
2021-08-31 13:01:48 +0800

    Move dired config to default

--- cats/+default.el
+++ cats/+default.el
@@ -67,3 +67,17 @@
                   ;; Prefix tramp autosaves to prevent conflicts with local ones
                   (concat auto-save-list-file-prefix "tramp-\\2") t)
             (list ".*" auto-save-list-file-prefix t)))
+
+;;; dired
+(setq dired-dwim-target t)
+
+(let ((args (list "-ahlv" "--group-directories-first")))
+  (when IS-BSD
+    ;; Use GNU ls as `gls' from `coreutils' if available. Add `(setq
+    ;; dired-use-ls-dired nil)' to your config to suppress the Dired warning
+    ;; when not using GNU ls.
+    (if-let (gls (executable-find "gls"))
+        (setq insert-directory-program gls)
+      ;; BSD ls doesn't support --group-directories-first
+      (setq args (list (car args)))))
+  (setq dired-listing-switches (string-join args " ")))
--- cats/+diredx.el
+++ cats/+diredx.el
@@ -1,14 +1,3 @@
-(let ((args (list "-ahlv" "--group-directories-first")))
-  (when IS-BSD
-    ;; Use GNU ls as `gls' from `coreutils' if available. Add `(setq
-    ;; dired-use-ls-dired nil)' to your config to suppress the Dired warning
-    ;; when not using GNU ls.
-    (if-let (gls (executable-find "gls"))
-        (setq insert-directory-program gls)
-      ;; BSD ls doesn't support --group-directories-first
-      (setq args (list (car args)))))
-  (setq dired-listing-switches (string-join args " ")))
-
 (add-hook 'dired-load-hook
           (lambda ()
 	    ;; Bind dired-x-find-file.
