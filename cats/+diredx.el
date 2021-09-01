cb28905dcb8856c179ab2811b0c6bb2e8549df6b
Asuna <chuxubank@qq.com>
2021-09-01 21:39:52 +0800

    Add dired-jump autoload

--- cats/+diredx.el
+++ cats/+diredx.el
@@ -13,3 +13,12 @@
 		    (concat dired-omit-files "\\|^ntuser\\(\\.dat\\|\\.ini\\).*")))
             (dired-omit-mode 1)
             ))
+
+(autoload 'dired-jump "dired-x"
+  "Jump to Dired buffer corresponding to current buffer." t)
+
+(autoload 'dired-jump-other-window "dired-x"
+  "Like \\[dired-jump] (dired-jump) but in other window." t)
+
+(define-key global-map "\C-x\C-j" 'dired-jump)
+(define-key global-map "\C-x4\C-j" 'dired-jump-other-window)
