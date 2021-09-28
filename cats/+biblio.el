58af406201dc421c50b4fa41d9ff5fdb6cddf150
chuxubank <chuxubank@qq.com>
2021-09-28 16:44:20 +0800

    Integrate org-roam-bibtex with other org-cite packages

--- cats/+biblio.el
+++ cats/+biblio.el
@@ -1,6 +1,7 @@
 (defvar cat-default-bibliography-files '("~/Zotero/My Library.bib"))
 
-(setq reftex-default-bibliography cat-default-bibliography-files)
+(setq reftex-default-bibliography cat-default-bibliography-files
+      org-cite-global-bibliography cat-default-bibliography-files)
 
 (use-package bibtex-completion
   :defer t
@@ -19,16 +20,21 @@
   :ensure bibtex-actions
   :after oc
   :config
-  (setq org-cite-global-bibliography cat-default-bibliography-files
+  (setq bibtex-actions-at-point-function 'embark-act
 	org-cite-insert-processor 'oc-bibtex-actions
         org-cite-follow-processor 'oc-bibtex-actions))
 
 (use-package org-roam-bibtex
   :after org-roam
   :config
+  (require 'ox)
   (add-to-list
    'org-roam-capture-templates
-   '("r" "bibliography reference" plain "%?"
+   '("r" "bibliography reference" plain
+     (file "org-roam-bibtex-template.org")
      :if-new
      (file+head "ref/${citekey}.org" "#+title: ${title}\n")
-     :unnarrowed t)))
+     :unnarrowed t))
+  (org-roam-bibtex-mode +1))
+
+(define-key global-map (kbd "C-c n") #'org-noter)
