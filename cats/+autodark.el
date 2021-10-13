29bc6bb443c3af64df85d97560694f03f3ebf07b
Asuna <chuxubank@qq.com>
2021-10-13 19:12:41 +0800

    Fix autodark on macOS

--- cats/+autodark.el
+++ cats/+autodark.el
@@ -2,7 +2,9 @@
   (when IS-WINDOWS
     (string-equal "0" (string-trim (shell-command-to-string "powershell -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
   (when IS-LINUX
-    (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme"))))
+    (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme")))
+  (when IS-MAC
+    (string-match-p "Dark" (shell-command-to-string "defaults read -g AppleInterfaceStyle"))))
 
 (add-hook 'after-init-hook (lambda ()
 			     (if (is-dark-mode)
