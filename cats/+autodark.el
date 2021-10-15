cfb5cda9026121215863c5f9f4e42fd2b64561ce
chuxubank <chuxubank@qq.com>
2021-10-15 09:29:45 +0800

    Fix dark mode function logic

--- cats/+autodark.el
+++ cats/+autodark.el
@@ -1,12 +1,10 @@
-(defun is-dark-mode()
-  (when IS-WINDOWS
-    (string-equal "0" (string-trim (shell-command-to-string "powershell -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
-  (when IS-LINUX
-    (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme")))
-  (when IS-MAC
-    (string-match-p "Dark" (shell-command-to-string "defaults read -g AppleInterfaceStyle"))))
+(defun cat-is-dark-mode()
+  (cond
+   (IS-WINDOWS (string-equal "0" (string-trim (shell-command-to-string "powershell -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
+   (IS-LINUX   (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme")))
+   (IS-MAC     (string-match-p "Dark" (shell-command-to-string "defaults read -g AppleInterfaceStyle")))))
 
 (add-hook 'after-init-hook (lambda ()
-			     (if (is-dark-mode)
+			     (if (cat-is-dark-mode)
 				 (nano-dark)
 			       (nano-light))))
