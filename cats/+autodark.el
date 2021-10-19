20c0e6d5de9e9a873446c858074b940a8bc8c9de
Misaka <chuxubank@qq.com>
2021-10-19 22:04:12 +0800

    Fix wsl gtk theme

--- cats/+autodark.el
+++ cats/+autodark.el
@@ -1,6 +1,7 @@
 (defun cat-is-dark-mode()
   (cond
-   ((or IS-WSL IS-WINDOWS) (string-equal "0" (string-trim (shell-command-to-string "powershell.exe -WindowStyle Hidden -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
+   (IS-WSL (string-match-p "-Darker" (getenv "GTK_THEME")))
+   (IS-WINDOWS (string-equal "0" (string-trim (shell-command-to-string "powershell.exe -WindowStyle Hidden -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
    (IS-LINUX   (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme")))
    (IS-MAC     (string-match-p "Dark" (shell-command-to-string "defaults read -g AppleInterfaceStyle")))))
 
