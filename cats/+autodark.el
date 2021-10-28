60faf77f52d2c18614aa5eaf23ce137ba2c178f8
Asuna <chuxubank@qq.com>
2021-10-28 16:37:45 +0800

    Improve auto-dark on macOS
    Ref https://github.com/ksixty/emacs-mac-follow-appearance

--- cats/+autodark.el
+++ cats/+autodark.el
@@ -1,11 +1,16 @@
-(defun cat-is-dark-mode()
+(defun cat-dark-mode-p ()
   (cond
-   (IS-WSL (string-match-p "-Darker" (getenv "GTK_THEME")))
-   (IS-WINDOWS (string-equal "0" (string-trim (shell-command-to-string "powershell.exe -WindowStyle Hidden -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
+   (IS-WSL     (string-match-p "-Darker" (getenv "GTK_THEME")))
+   (IS-WINDOWS (string= "0" (string-trim (shell-command-to-string "powershell.exe -WindowStyle Hidden -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
    (IS-LINUX   (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme")))
-   (IS-MAC     (string-match-p "Dark" (shell-command-to-string "defaults read -g AppleInterfaceStyle")))))
+   (IS-MAC     (string= (plist-get (mac-application-state) :appearance) "NSAppearanceNameDarkAqua"))))
 
-(add-hook 'after-init-hook (lambda ()
-			     (if (cat-is-dark-mode)
-				 (nano-dark)
-			       (nano-light))))
+(defun cat-load-theme ()
+  (interactive)
+  (if (cat-dark-mode-p)
+      (nano-dark)
+    (nano-light)))
+
+(add-hook 'after-init-hook #'cat-load-theme)
+
+(define-key mac-apple-event-map [application-kvo effectiveAppearance] #'cat-load-theme)
