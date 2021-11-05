57e2b4a6b208d29a7dd13c852e0043a6825b236f
Asuna <chuxubank@qq.com>
2021-11-05 18:06:58 +0800

    Add osx-location

--- cats/+macos.el
+++ cats/+macos.el
@@ -4,3 +4,11 @@
            (display-graphic-p))
        (ns-auto-titlebar-mode +1)))
 
+(use-package osx-location
+  :defer t
+  :config
+  (add-hook 'osx-location-changed-hook
+            (lambda ()
+              (setq calendar-latitude osx-location-latitude
+                    calendar-longitude osx-location-longitude
+                    calendar-location-name (format "%s, %s" osx-location-latitude osx-location-longitude)))))
