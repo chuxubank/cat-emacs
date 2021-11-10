0796fc22d6ea1b06366c98cb22d628d739ad29e8
Asuna <chuxubank@qq.com>
2021-11-10 18:08:46 +0800

    Remove pdf view with nano modeline

--- cats/+pdf.el
+++ cats/+pdf.el
@@ -3,24 +3,7 @@
   :init
   (setq pdf-view-use-scaling t)
   :config
-  (pdf-tools-install)
-  (defun nano-modeline-pdf-view-mode ()
-    (let ((buffer-name (format-mode-line "%b"))
-	  (mode-name   (nano-mode-name))
-	  (branch      (vc-branch))
-	  (page-number (concat
-			(number-to-string (pdf-view-current-page)) "/"
-			(or (ignore-errors
-			      (number-to-string (pdf-cache-number-of-pages)))
-			    "???"))))
-      (nano-modeline-compose
-       "RO"
-       buffer-name
-       (concat "("
-	       mode-name
-	       (if branch (concat ", " (propertize branch 'face 'italic)))
-	       ")" )
-       page-number))))
+  (pdf-tools-install))
 
 (add-hook 'pdf-view-mode-hook (lambda () (pdf-view-themed-minor-mode 1)))
 
