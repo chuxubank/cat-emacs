4f9d365454bb3cd0fb4831d69240a788b524078f
Asuna <chuxubank@qq.com>
2021-11-11 10:58:20 +0800

    Add org-drill configs

--- cats/+org-srs.el
+++ cats/+org-srs.el
@@ -1,7 +1,10 @@
 (use-package org-drill
   :defer t
   :custom
-  (org-drill-scope 'directory))
+  (org-drill-scope 'directory)
+  (org-drill-spaced-repetition-algorithm 'simple8)
+  (org-drill-add-random-noise-to-intervals-p t)
+  (org-drill-adjust-intervals-for-early-and-late-repetitions-p t))
 
 (use-package org-fc
   :disabled
@@ -20,5 +23,8 @@
   :after org)
 
 (with-eval-after-load 'org
+  (define-key org-mode-map (kbd "C-c n d d") #'org-drill)
+  (define-key org-mode-map (kbd "C-c n d a") #'org-drill-again)
+  (define-key org-mode-map (kbd "C-c n d c") #'org-drill-cram)
   (define-key org-mode-map (kbd "C-c n a s") #'org-anki-sync-entry)
   (define-key org-mode-map (kbd "C-c n a c") #'org-anki-cloze-dwim))
