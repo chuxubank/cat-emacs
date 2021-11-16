7bb22c8f7d40e95c06d98dd93ba1217bf2d1e310
Asuna <chuxubank@qq.com>
2021-11-16 10:33:39 +0800

    Add js mode with smartparens

--- cats/+smartparens.el
+++ cats/+smartparens.el
@@ -6,5 +6,5 @@
 (dolist (h '(c-mode-hook org-mode-hook))
   (add-hook h 'smartparens-mode))
 
-(dolist (h '(lisp-mode-hook lisp-interaction-mode-hook emacs-lisp-mode-hook))
+(dolist (h '(lisp-mode-hook lisp-interaction-mode-hook emacs-lisp-mode-hook js-mode-hook))
   (add-hook h 'smartparens-strict-mode))
