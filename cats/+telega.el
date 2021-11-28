da926f28586a12c58e7fdfe977b45bf81196553e
Asuna <chuxubank@qq.com>
2021-11-28 15:51:55 +0800

    Add telega mnz mode

--- cats/+telega.el
+++ cats/+telega.el
@@ -24,6 +24,8 @@
 (define-key global-map (kbd "C-c t k") #'telega-kill)
 
 (with-eval-after-load 'telega
+  (require 'telega-mnz)
+  (global-telega-mnz-mode)
   (when cat-alt-code-font
     (set-face-attribute 'telega-entity-type-pre nil :family cat-alt-code-font)
     (set-face-attribute 'telega-entity-type-code nil :family cat-alt-code-font)
