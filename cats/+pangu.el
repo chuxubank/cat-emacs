b8390ea02768db3097d2ff6faf05889a6e5f6294
Asuna <chuxubank@qq.com>
2021-11-29 18:05:20 +0800

    Fix pangu spacing with org-noter

--- cats/+pangu.el
+++ cats/+pangu.el
@@ -3,4 +3,13 @@
 (use-package pangu-spacing
   :hook (text-mode . pangu-spacing-mode)
   :config
-  (setq pangu-spacing-real-insert-separtor t))
+  (setq pangu-spacing-real-insert-separtor t)
+  (defun pangu-spacing-org-mode-at-special-region ()
+    (interactive)
+    (let ((element (org-element-at-point)))
+      (when (or (member (org-element-type element)
+			'(src-block keyword example-block export-block
+                                    latex-environment planning node-property))
+		(member (car (org-element-context element))
+			'(inline-src-block timestamp link code verbatim)))
+	t))))
