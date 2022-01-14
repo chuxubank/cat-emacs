ac9e55e212b1141d2f51dd9ee7ab19c28a858d5d
Xu Chu <xu.chu@assaabloy.com>
2022-01-14 10:01:49 +0800

    Fix nxml-mode with pangu

--- cats/+pangu.el
+++ cats/+pangu.el
@@ -4,6 +4,7 @@
   :hook (text-mode . pangu-spacing-mode)
   :config
   (setq pangu-spacing-real-insert-separtor t)
+  ;; Add `note-property'
   (defun pangu-spacing-org-mode-at-special-region ()
     (interactive)
     (let ((element (org-element-at-point)))
@@ -13,3 +14,8 @@
 		(member (car (org-element-context element))
 			'(inline-src-block timestamp link code verbatim)))
 	t))))
+
+(defun +pangu-spacing-disable ()
+  (pangu-spacing-mode -1))
+
+(add-hook 'nxml-mode-hook #'+pangu-spacing-disable)
