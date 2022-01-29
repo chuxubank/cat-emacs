872b317ce39c6c24e853bfbef157e106db6b9171
Misaka <chuxubank@qq.com>
2022-01-29 14:14:32 +0800

    Add benchmark

---
+++ cats/+benchmark.el
@@ -0,0 +1,6 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package benchmark-init
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
