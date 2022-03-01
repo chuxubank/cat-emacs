57cfb544d5cacabcc1667a2018b92de6d82a6d86
Misaka <chuxubank@qq.com>
2022-03-01 17:56:35 +0800

    Add keybind for magit-list-repositories

--- cats/+magit.el
+++ cats/+magit.el
@@ -2,14 +2,16 @@
 
 (use-package magit
   :defer t
+  :bind
+  ("C-c p l" . #'magit-list-repositories)
   :init
   (setq transient-levels-file (concat cat-etc-dir "transient/levels")
         transient-values-file (concat cat-etc-dir "transient/values")
-        transient-history-file (concat cat-etc-dir "transient/history"))
+        transient-history-file (concat cat-etc-dir "transient/history")
+	magit-repository-directories '(("~/Developer/" . 3)))
   :config
   (setq magit-diff-refine-hunk t
-	magit-diff-refine-ignore-whitespace nil
-	magit-repository-directories '(("~/Developer/" . 3))))
+	magit-diff-refine-ignore-whitespace nil))
 
 (use-package forge
   :after magit
