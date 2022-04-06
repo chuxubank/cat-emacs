9ca5f529b67c3b2a2a8658515627e24fa16cc2f3
Xu Chu <xu.chu@assaabloy.com>
2022-04-06 14:34:40 +0800

    Disable kotlin lsp

--- cats/+lsp.el
+++ cats/+lsp.el
@@ -4,12 +4,15 @@
   :init
   (setq lsp-keymap-prefix "C-c C-l"
 	lsp-session-file (concat cat-etc-dir "lsp-session"))
-  :hook ((kotlin-mode . lsp)
-	 (dart-mode . lsp)
-	 (lsp-mode . lsp-enable-which-key-integration))
+  :hook
+  ;; (kotlin-mode . lsp)
+  (dart-mode . lsp)
+  (lsp-mode . lsp-enable-which-key-integration)
   :commands lsp)
 
 (use-package lsp-ui
-  :commands lsp-ui-mode)
+  :commands lsp-ui-mode
+  :custom
+  (lsp-headerline-breadcrumb-icons-enable nil))
 
 (setq treemacs-persist-file (concat cat-cache-dir "treemacs"))
