19b721d2e79ecfbe2c2001f81ac71163e49ca76e
Xu Chu <xu.chu@assaabloy.com>
2022-04-27 09:56:15 +0800

    Add ediff copy both to C

--- cats/+default.el
+++ cats/+default.el
@@ -109,6 +109,16 @@
 ;;; ediff
 (setq ediff-diff-options "-w" ; turn off whitespace checking
       ediff-split-window-function #'split-window-horizontally)
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
 
 ;;; so long
 (global-so-long-mode 1)
