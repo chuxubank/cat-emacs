90e9d1b1c8bb8990b939b882016bc96ae9bb588f
Misaka <chuxubank@qq.com>
2022-05-11 15:32:02 +0800

    Fix flycheck

--- cats/+flycheck.el
+++ cats/+flycheck.el
@@ -6,8 +6,10 @@
   (setq flycheck-emacs-lisp-load-path 'inherit))
 
 (use-package flycheck-pos-tip
+  :after flycheck
   :hook (flycheck-mode . flycheck-pos-tip-mode))
 
 (use-package flycheck-posframe
   :disabled
+  :after flycheck
   :hook (flycheck-mode . flycheck-posframe-mode))
