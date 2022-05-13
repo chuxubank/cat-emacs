81b2e88254888618c9d7d01a8d9a66c8b624d149
Xu Chu <xu.chu@assaabloy.com>
2022-05-13 17:57:46 +0800

    Simplify caddyfile mode

--- cats/+caddy.el
+++ cats/+caddy.el
@@ -1,13 +1,4 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package caddyfile-mode
-  :mode
-  (("Caddyfile\\'" . caddyfile-mode)
-   ("caddy\\.conf\\'" . caddyfile-mode)))
-
-
-(defun cat-caddyfile-hook ()
-  (setq-local tab-width 4)
-  (setq-local indent-tabs-mode nil))
-
-(add-hook 'caddyfile-mode-hook #'cat-caddyfile-hook)
+  :mode "Caddyfile\\'")
