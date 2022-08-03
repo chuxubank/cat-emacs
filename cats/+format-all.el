801518cc9be1eacba0618b9782578d515505c48a
Misaka <chuxubank@qq.com>
2022-08-03 16:20:55 +0800

    Not enable format-all on all prog-mode

--- cats/+format-all.el
+++ cats/+format-all.el
@@ -3,5 +3,11 @@
 (use-package format-all
   :defer t)
 
-(add-hook 'prog-mode-hook 'format-all-mode)
+(dolist (h '(c-mode-hook
+	     lisp-mode-hook
+	     lisp-interaction-mode-hook
+	     emacs-lisp-mode-hook
+	     js-mode-hook
+	     bibtex-mode-hook))
+  (add-hook h 'format-all-mode))
 (add-hook 'format-all-mode-hook #'format-all-ensure-formatter)
