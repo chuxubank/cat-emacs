15117dcd54ce6336ad86b5f78ca9a0e9bcfa76c6
Misaka <chuxubank@qq.com>
2022-09-11 09:45:13 +0800

    Make repository directory more deep

--- cats/+magit.el
+++ cats/+magit.el
@@ -6,7 +6,7 @@
   (setq transient-levels-file (concat cat-etc-dir "transient/levels")
         transient-values-file (concat cat-etc-dir "transient/values")
         transient-history-file (concat cat-etc-dir "transient/history")
-	magit-repository-directories '(("~/Developer/" . 3)))
+	magit-repository-directories '(("~/Developer/" . 5)))
   :config
   (setq magit-diff-refine-hunk t
 	magit-diff-refine-ignore-whitespace nil))
