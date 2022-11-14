95d1710fed8dfbdfdcb8e5649a66e955ffc726b4
Misaka <chuxubank@qq.com>
2022-11-15 01:49:40 +0800

    Use vertico-prescient-mode instead of self config

--- cats/+prescient.el
+++ cats/+prescient.el
@@ -9,20 +9,8 @@
   (selectrum-prescient-mode 1))
 
 (when (featurep 'vertico)
-  ;; Enable the completion style.
-  (setq completion-styles '(prescient basic)
-	completion-category-overrides '((file (styles basic-remote partial-completion))))
-
-  ;; Use `prescient-completion-sort' after filtering.
-  (setq vertico-sort-function #'prescient-completion-sort)
-
-  (defun vertico-prescient-remember ()
-    "Remember the chosen candidate with Prescient."
-    (when (>= vertico--index 0)
-      (prescient-remember
-       (substring-no-properties
-	(nth vertico--index vertico--candidates)))))
-  (advice-add #'vertico-insert :after #'vertico-prescient-remember))
+  (use-package vertico-prescient)
+  (vertico-prescient-mode 1))
 
 (setq prescient-save-file (expand-file-name "prescient-save.el" cat-cache-dir))
 (prescient-persist-mode 1)
