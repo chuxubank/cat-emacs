aca6aa547b50dc84b3dbbc315904f8feb9ed1081
Misaka <chuxubank@qq.com>
2022-11-21 20:47:40 +0800

    Use package instead of when condition

--- cats/+prescient.el
+++ cats/+prescient.el
@@ -1,15 +1,18 @@
 ;; -*- lexical-binding: t; -*-
 
-(when (featurep 'company)
-  (use-package company-prescient)
+(use-package company-prescient
+  :if (featurep 'company)
+  :config
   (company-prescient-mode 1))
 
-(when (featurep 'selectrum)
-  (use-package selectrum-prescient)
+(use-package selectrum-prescient
+  :if (featurep 'selectrum)
+  :config
   (selectrum-prescient-mode 1))
 
-(when (featurep 'vertico)
-  (use-package vertico-prescient)
+(use-package vertico-prescient
+  :if (featurep 'vertico)
+  :config
   (vertico-prescient-mode 1))
 
 (setq prescient-save-file (expand-file-name "prescient-save.el" cat-cache-dir))
