e016e4f85e98d70c48f982849ecf26be6c963368
Misaka <chuxubank@qq.com>
2023-02-16 09:22:42 +0800

    Fix plantuml indent level

--- cats/+plantuml.el
+++ cats/+plantuml.el
@@ -6,15 +6,16 @@
   :defer t
   :custom
   (plantuml-default-exec-mode 'executable)
+  (plantuml-indent-level 4)
   :config
   (defun plantuml-executable-start-process (buf)
-  "Run PlantUML as an Emacs process and puts the output into the given buffer (as BUF)."
-  (apply #'start-process
-         "PLANTUML" buf plantuml-executable-path
-         `(,@plantuml-executable-args
-           ,(plantuml-jar-output-type-opt plantuml-output-type)
-	   ,(if (+dark-mode-p) plantuml-dark-arg "")
-           "-p"))))
+    "Run PlantUML as an Emacs process and puts the output into the given buffer (as BUF)."
+    (apply #'start-process
+           "PLANTUML" buf plantuml-executable-path
+           `(,@plantuml-executable-args
+             ,(plantuml-jar-output-type-opt plantuml-output-type)
+	     ,(if (+dark-mode-p) plantuml-dark-arg "")
+             "-p"))))
 
 (use-package flycheck-plantuml
   :after plantuml-mode
