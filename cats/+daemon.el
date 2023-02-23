f0628f857a3ce36ec05c143b7c92bab85f289e88
Misaka <chuxubank@qq.com>
2023-02-23 10:33:33 +0800

    Fix daemon font set

--- cats/+daemon.el
+++ cats/+daemon.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+(defvar cat-font-load nil
+  "Whether cat font has been loaded.")
+
 ;; see https://github.com/railwaycat/homebrew-emacsmacport/issues/52
 (use-package mac-pseudo-daemon
   :when IS-MACPORT
@@ -12,6 +15,9 @@
   (exec-path-from-shell-initialize))
 
 (defun cat-client-frame-config ()
-  (select-frame-set-input-focus (selected-frame)))
+  (select-frame-set-input-focus (selected-frame))
+  (when (not cat-font-load)
+    (cat! "+font")
+    (setq cat-font-load t)))
 
 (add-hook 'server-after-make-frame-hook #'cat-client-frame-config)
