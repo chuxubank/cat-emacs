90c31c3f140589f1f88b5360b9117a40fcbfad47
Misaka <chuxubank@qq.com>
2023-02-28 14:49:32 +0800

    Fix plantuml server url

--- cats/+plantuml.el
+++ cats/+plantuml.el
@@ -15,7 +15,30 @@
            `(,@plantuml-executable-args
              ,(plantuml-jar-output-type-opt plantuml-output-type)
 	     ,(if (+dark-mode-p) plantuml-dark-arg "")
-             "-p"))))
+             "-p")))
+
+  (defun hex-encode (str)
+    (string-join (mapcar (lambda (c) (format "%02x" c)) (string-as-unibyte str))))
+
+  (defun plantuml-server-encode-url (string)
+    "Encode the string STRING into a URL suitable for PlantUML server interactions."
+    (let* ((encoded-string (hex-encode string)))
+      (concat plantuml-server-url "/" plantuml-output-type "/~h" encoded-string)))
+
+  (defun +plantuml-compute-url (&optional file)
+    "Compute the PlantUML server URL for the current buffer or FILE."
+    (interactive)
+    (let* ((string (buffer-string))
+	   (in-file (or file (buffer-file-name)
+			(make-temp-file "plantuml-" nil ".puml"
+					(if (string-prefix-p "@start" string t) string
+					  (format "@startuml\n%s\n@enduml" string)))))
+	   (data (string-trim (shell-command-to-string (format "%s -computeurl %s" plantuml-executable-path in-file))))
+	   (url (concat plantuml-server-url "/" plantuml-output-type "/" data)))
+      (when (string-empty-p data) (error "Failed to compute URL"))
+      (kill-new url)
+      (message "Copied PlantUML server URL to kill ring")
+      url)))
 
 (use-package flycheck-plantuml
   :after plantuml-mode
@@ -24,3 +47,6 @@
 
 (with-eval-after-load 'ob-plantuml
   (setq org-plantuml-exec-mode 'plantuml))
+
+(with-eval-after-load 'plantuml-mode
+  (define-key plantuml-mode-map (kbd "C-c C-l") #'+plantuml-compute-url))
