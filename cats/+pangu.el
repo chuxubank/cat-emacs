06165e2bd7ee7b46b1770c3ee4b5182fc65e4c40
Misaka <chuxubank@qq.com>
2023-03-09 14:15:02 +0800

    Fix pangu in org table

--- cats/+pangu.el
+++ cats/+pangu.el
@@ -4,15 +4,25 @@
   :hook (text-mode . pangu-spacing-mode)
   :config
   (setq pangu-spacing-real-insert-separtor t)
-  ;; Add `note-property'
+  ;; Add `note-property' and `table-row'
   (defun pangu-spacing-org-mode-at-special-region ()
     (interactive)
     (let ((element (org-element-at-point)))
       (when (or (member (org-element-type element)
-			'(src-block keyword example-block export-block
-                                    latex-environment planning node-property))
+			'(src-block
+			  keyword
+			  example-block
+			  export-block
+			  latex-environment
+			  planning
+			  node-property
+			  table-row))
 		(member (car (org-element-context element))
-			'(inline-src-block timestamp link code verbatim)))
+			'(inline-src-block
+			  timestamp
+			  link
+			  code
+			  verbatim)))
 	t))))
 
 (defun +pangu-spacing-disable ()
