6837d7611f438f12e9f5db1bd6861939f7418f66
Misaka <chuxubank@qq.com>
2023-06-01 09:50:15 +0800

    Add beancount config

--- cats/+beancount.el
+++ cats/+beancount.el
@@ -2,4 +2,10 @@
 
 (use-package beancount
   :straight (beancount :host github :repo "beancount/beancount-mode")
-  :mode "\\.beancount\\'")
+  :init
+  (setq beancount-mode-map-prefix [(control c) (control c)])
+  (add-hook 'beancount-mode-hook #'outline-minor-mode)
+  :mode ("\\.beancount\\'" . beancount-mode)
+  :config
+  (define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
+  (define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading))
