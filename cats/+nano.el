5baf3432d5151b3f7d3402cfad8f4da8c1f6a871
Misaka <chuxubank@qq.com>
2023-06-09 23:07:25 +0800

    Update nano-modeline config

--- cats/+nano.el
+++ cats/+nano.el
@@ -20,10 +20,22 @@
 
 (use-package nano-modeline
   :custom
-  (nano-modeline-prefix-padding t)
-  (nano-modeline-position 'bottom)
+  (nano-modeline-position 'nano-modeline-footer)
   :config
-  (nano-modeline-mode))
+  (add-hook 'prog-mode-hook            #'nano-modeline-prog-mode)
+  (add-hook 'text-mode-hook            #'nano-modeline-text-mode)
+  (add-hook 'org-mode-hook             #'nano-modeline-org-mode)
+  (add-hook 'pdf-view-mode-hook        #'nano-modeline-pdf-mode)
+  (add-hook 'mu4e-headers-mode-hook    #'nano-modeline-mu4e-headers-mode)
+  (add-hook 'mu4e-view-mode-hook       #'nano-modeline-mu4e-message-mode)
+  (add-hook 'elfeed-show-mode-hook     #'nano-modeline-elfeed-entry-mode)
+  (add-hook 'elfeed-search-mode-hook   #'nano-modeline-elfeed-search-mode)
+  (add-hook 'term-mode-hook            #'nano-modeline-term-mode)
+  (add-hook 'xwidget-webkit-mode-hook  #'nano-modeline-xwidget-mode)
+  (add-hook 'messages-buffer-mode-hook #'nano-modeline-message-mode)
+  (add-hook 'org-capture-mode-hook     #'nano-modeline-org-capture-mode)
+  (add-hook 'org-agenda-mode-hook      #'nano-modeline-org-agenda-mode)
+  (nano-modeline-text-mode t))
 
 (use-package nano-minibuffer
   :disabled
