8d2f705f06109b699a563ec92c83aa94f9bbca7b
Misaka <chuxubank@qq.com>
2023-07-02 00:09:12 +0800

    Change doom default theme

--- cats/+doom.el
+++ cats/+doom.el
@@ -1,14 +1,19 @@
 ;; -*- lexical-binding: t; -*-
 
+(defun doom-dark-theme ()
+  (load-theme 'doom-opera t))
+(defun doom-light-theme ()
+  (load-theme 'doom-opera-light t))
+
 (use-package doom-themes
   :config
   (doom-themes-neotree-config)
   (doom-themes-treemacs-config)
-  (doom-themes-org-config)
-  (defun doom-dark-theme ()
-      (load-theme 'doom-nord t))
-  (defun doom-light-theme ()
-      (load-theme 'doom-nord-light t)))
+  (doom-themes-org-config))
+
+(use-package kaolin-themes
+  :config
+  (kaolin-treemacs-theme))
 
 (use-package doom-modeline
   :hook after-init)
