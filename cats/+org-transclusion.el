a94b99dbafd0f0e3e471262d3a58d03f921efc57
Misaka <chuxubank@qq.com>
2023-07-10 19:57:36 +0800

    Remove org-transclusion straight config

--- cats/+org-transclusion.el
+++ cats/+org-transclusion.el
@@ -1,10 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package org-transclusion
-  :straight (org-transclusion
-	     :host github
-	     :repo "nobiot/org-transclusion"
-	     :files ("*.el"))
   :after org
   :custom
   (org-transclusion-extensions '(org-transclusion-src-lines
