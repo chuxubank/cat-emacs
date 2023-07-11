3d28eb982abae949ebfd5b6445a2c0dca4879b15
Misaka <chuxubank@qq.com>
2023-07-11 21:35:50 +0800

    Set beancount not use ido

--- cats/+beancount.el
+++ cats/+beancount.el
@@ -6,6 +6,8 @@
   (setq beancount-mode-map-prefix [(control c) (control c)])
   (add-hook 'beancount-mode-hook #'outline-minor-mode)
   :mode ("\\.beancount\\'" . beancount-mode)
+  :custom
+  (beancount-use-ido nil)
   :config
   (define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
   (define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading)
