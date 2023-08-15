acf6c4c478ebce14a981cbd34eb72b945a912f94
Misaka <chuxubank@qq.com>
2023-08-15 09:10:25 +0800

    Add custom log formats

--- cats/+log.el
+++ cats/+log.el
@@ -5,4 +5,22 @@
   :custom
   (datetime-timezone 'Asia/Shanghai)
   (logview-cache-filename (concat cat-cache-dir "logview-cache.extmap"))
-  (logview-views-file (concat cat-etc-dir "logview.views")))
+  (logview-views-file (concat cat-etc-dir "logview.views"))
+  (logview-additional-timestamp-formats
+   '(("LogCat"
+      (java-pattern . "MM-dd HH:mm:ss.SSS"))))
+  (logview-additional-level-mappings
+   '(("LogCat"
+      (error "E" "F" "S")
+      (warning "W")
+      (information "I")
+      (debug "D")
+      (trace "V"))))
+  (logview-additional-submodes
+   '(("Luna"
+      (format . "TIMESTAMP IGNORED LEVEL T: THREAD NAME - MESSAGE")
+      (levels . "Logback"))
+     ("LogCat"
+      (format . "TIMESTAMP IGNORED THREAD LEVEL NAME: MESSAGE")
+      (levels . "LogCat")
+      (timestamp "LogCat")))))
