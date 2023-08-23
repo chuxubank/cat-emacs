4f5cec6c613503bdd73e4770827925eaa5d6869d
Misaka <chuxubank@qq.com>
2023-08-23 23:11:18 +0800

    Enable apheleia-global-mode after init

--- cats/+apheleia.el
+++ cats/+apheleia.el
@@ -1,7 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package apheleia
+  :hook (after-init . apheleia-global-mode)
   :custom
-  (apheleia-remote-algorithm 'local)
-  :config
-  (apheleia-global-mode +1))
+  (apheleia-remote-algorithm 'local))
