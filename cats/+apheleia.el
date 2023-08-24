9397d7e8fcc898011c74e6ec4f1f77a3b146a21d
Misaka <chuxubank@qq.com>
2023-08-24 10:16:13 +0800

    Make latexindent load local settings

--- cats/+apheleia.el
+++ cats/+apheleia.el
@@ -3,4 +3,7 @@
 (use-package apheleia
   :hook (after-init . apheleia-global-mode)
   :custom
-  (apheleia-remote-algorithm 'local))
+  (apheleia-remote-algorithm 'local)
+  :config
+  (setf (alist-get 'latexindent apheleia-formatters)
+        '("latexindent" "-l" "--logfile=/dev/null")))
