3d17767236c78130f9496505f003b1bb43e6fb4d
Misaka <chuxubank@qq.com>
2023-10-07 17:31:09 +0800

    Modify +org-todo-cancel face

--- cats/+org-todo.el
+++ cats/+org-todo.el
@@ -11,7 +11,7 @@
   (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
   (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
   (custom-declare-face '+org-todo-onhold  '((t (:inherit (bold warning org-todo)))) "")
-  (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) ""))
+  (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold org-done) :strike-through t))) ""))
 (setq org-todo-keywords
       '((sequence
          "TODO(t)"  ; A task that needs doing & is ready to do
