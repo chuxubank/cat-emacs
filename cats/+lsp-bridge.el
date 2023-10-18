975e0001b81076f92e0394f3621049c39e7a0a70
Misaka <chuxubank@qq.com>
2023-10-18 16:50:43 +0800

    Improve lsp-bridge

--- cats/+lsp-bridge.el
+++ cats/+lsp-bridge.el
@@ -11,4 +11,10 @@
   :custom
   (codeium-bridge-folder (expand-file-name cat-codeium-dir))
   (acm-enable-codeium t)
-  (acm-backend-codeium-api-key-path cat-codeium-api-key-file))
+  (acm-backend-codeium-api-key-path cat-codeium-api-key-file)
+  :config
+  (global-corfu-mode -1))
+
+(use-package flymake-bridge
+  :straight (flymake-bridge :host github :repo "liuyinz/flymake-bridge")
+  :hook (lsp-bridge-mode . flymake-bridge-setup))
