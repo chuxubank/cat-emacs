5cb4dfcde731240c67b32dd7df43586bea259c25
Misaka <chuxubank@qq.com>
2023-11-15 19:55:54 +0800

    Update magit config with use-package and change lighter for blame mode

--- cats/+magit.el
+++ cats/+magit.el
@@ -1,17 +1,21 @@
 ;; -*- lexical-binding: t; -*-
 
+(use-package transient
+  :defer t
+  :custom
+  (transient-levels-file (concat cat-etc-dir "transient/levels"))
+  (transient-values-file (concat cat-etc-dir "transient/values"))
+  (transient-history-file (concat cat-etc-dir "transient/history")))
+
 (use-package magit
   :defer t
-  :init
-  (setq transient-levels-file (concat cat-etc-dir "transient/levels")
-        transient-values-file (concat cat-etc-dir "transient/values")
-        transient-history-file (concat cat-etc-dir "transient/history")
-        magit-repository-directories '(("~/Developer/" . 5)))
-  :config
-  (setq magit-diff-refine-hunk t
-        magit-diff-refine-ignore-whitespace nil))
+  :custom
+  (magit-blame-mode-lighter " ")
+  (magit-repository-directories '(("~/Developer/" . 5)))
+  (magit-diff-refine-hunk t)
+  (magit-diff-refine-ignore-whitespace nil))
 
 (use-package forge
   :after magit
-  :config
-  (setq forge-database-file (concat cat-etc-dir "forge.db")))
+  :custom
+  (forge-database-file (concat cat-etc-dir "forge.db")))
