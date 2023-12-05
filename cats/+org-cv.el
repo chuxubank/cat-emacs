535cc3ccf8a6af5f5251d07b1ee2adbd1eb2e176
Misaka <chuxubank@qq.com>
2023-12-05 10:13:32 +0800

    Fix org-cv

--- cats/+org-cv.el
+++ cats/+org-cv.el
@@ -1,7 +1,6 @@
-(use-package org-cv
-  :vc (:url "https://github.com/Titan-C/org-cv" :rev :newest)
+;; -*- lexical-binding: t; -*-
+
+(use-package ox-moderncv
+  :vc (:url "https://gitlab.com/Titan-C/org-cv" :rev :newest)
   :demand t
-  :after org
-  :config
-  (require 'ox-moderncv)
-  (require 'ox-awesomecv))
+  :after org)
