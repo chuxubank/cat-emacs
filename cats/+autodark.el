eb50c329026541e7bff5b84733d53cd5662473e3
Misaka <chuxubank@qq.com>
2023-12-12 13:46:07 +0800

    Fix autodark in term: fallback to macOS theme when in terminal

--- cats/+autodark.el
+++ cats/+autodark.el
@@ -24,7 +24,7 @@
    (IS-WSL     (string-match-p "-Darker" (getenv "GTK_THEME")))
    (IS-WINDOWS (string= "0" (string-trim (shell-command-to-string "powershell.exe -WindowStyle Hidden -C Get-ItemPropertyValue -Path HKCU://Software/Microsoft/Windows/CurrentVersion/Themes/Personalize -Name AppsUseLightTheme"))))
    (IS-LINUX   (string-match-p "-dark" (shell-command-to-string "gsettings get org.gnome.desktop.interface gtk-theme")))
-   (IS-MACPORT (string= (plist-get (mac-application-state) :appearance) "NSAppearanceNameDarkAqua"))
+   ((and IS-MACPORT (display-graphic-p)) (string= (plist-get (mac-application-state) :appearance) "NSAppearanceNameDarkAqua"))
    ((and IS-MACPLUS (display-graphic-p)) (eq 'dark ns-system-appearance))
    (IS-MAC     (string= "Dark" (string-trim (shell-command-to-string "defaults read -g AppleInterfaceStyle"))))))
 
