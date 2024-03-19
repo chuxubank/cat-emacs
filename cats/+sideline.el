5c5ea420b73144ba84e3b6174731a9afdebca6df
Misaka <chuxubank@qq.com>
2024-03-19 16:15:51 +0800

    Remove custom config for sideline
    https://github.com/emacs-sideline/sideline/pull/20

--- cats/+sideline.el
+++ cats/+sideline.el
@@ -4,29 +4,7 @@
   :delight
   :hook (after-init . global-sideline-mode)
   :custom
-  (sideline-display-backend-name t)
-  :config
-  (defun sideline--align-right (str offset)
-    "Align sideline STR from the right of the window.
-
-Argument OFFSET is additional calculation from the right alignment.
-Fixed version for `buffer-face-mode'."
-    (list (+
-           ;; If the sideline text is displayed without at least 1 pixel gap from the right fringe and
-           ;; overflow-newline-into-fringe is not true, emacs will line wrap it.
-           (if (and (display-graphic-p)
-                    (> (nth 1 (window-fringes)) 0)
-                    (not overflow-newline-into-fringe))
-               1
-             0)
-           (* (window-font-width)
-              (+ offset
-                 (if (display-graphic-p)
-                     ;; If right fringe deactivated add 1 offset
-                     (if (= 0 (nth 1 (window-fringes))) 1 0)
-                   1)
-                 (length str)))
-           ))))
+  (sideline-display-backend-name t))
 
 (use-package sideline-flycheck
   :hook (flycheck-mode . sideline-flycheck-setup)
