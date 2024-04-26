33b648806f8a2bc819b562272e504e52983d04d6
Misaka <chuxubank@qq.com>
2024-04-26 16:35:31 +0800

    Add import fold for kotlin and java

--- cats/+tree-sitter.el
+++ cats/+tree-sitter.el
@@ -15,11 +15,29 @@
 (use-package ts-docstr
   :pin jcs-elpa)
 
+(defun ts-fold-range-same (node offset prefix)
+  "Define fold range for same tag with PREFIX.
+
+For arguments NODE and OFFSET, see function `ts-fold-range-seq' for
+more information."
+  (save-excursion
+    (when-let* ((first-node (ts-fold--continuous-node-prefix node prefix nil))
+                (last-node (ts-fold--continuous-node-prefix node prefix t))
+                (prefix-len (length prefix))
+                (beg (+ (tsc-node-start-position first-node) prefix-len))
+                (end (tsc-node-end-position last-node)))
+      (ts-fold--cons-add (cons beg end) offset))))
+
 (use-package ts-fold
   :pin jcs-elpa
   :delight " "
   :hook
-  (after-init . global-ts-fold-indicators-mode))
+  (after-init . global-ts-fold-indicators-mode)
+  :config
+  (push '(import_list . (ts-fold-range-seq 6 -1)) (alist-get 'kotlin-mode ts-fold-range-alist))
+  (push '(import_declaration
+          . (lambda (node offset)
+              (ts-fold-range-same node offset "import "))) (alist-get 'java-mode ts-fold-range-alist)))
 
 (use-package treesit-auto
   :disabled
