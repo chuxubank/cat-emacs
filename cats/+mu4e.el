b3c84badc73c8fcda7de73cd427679ede680f88d
Misaka <chuxubank@qq.com>
2024-08-08 19:06:46 +0800

    Fix mu4e

--- cats/+mu4e.el
+++ cats/+mu4e.el
@@ -1,13 +1,17 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package mu4e
-  
+  :commands #'mu4e
+  :ensure-system-package
+  (mu)
+  (mbsync . isync)
   :ensure nil
   :custom
   (mu4e-get-mail-command "mbsync -a")
   (mu4e-use-fancy-chars t)
   (mu4e-sent-messages-behavior 'delete)
-  (shr-color-visible-luminance-min 80))
+  (mu4e-read-option-use-builtin nil)
+  (mu4e-completing-read-function 'completing-read))
 
 (with-eval-after-load 'selectrum
   (setq mu4e-completing-read-function #'selectrum-completing-read))
