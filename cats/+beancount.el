5e790e60b6edf2b48b475fe46a8aa3c00ae8c13c
Misaka <chuxubank@qq.com>
2024-09-25 08:19:18 +0800

    Add lsp to beancount-mode

--- cats/+beancount.el
+++ cats/+beancount.el
@@ -1,6 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package beancount
+  :ensure-system-package
+  (beancount-language-server . beancount-language-server)
   :pin jcs-elpa
   :init
   (setq beancount-mode-map-prefix [(control c) (control c)])
@@ -9,6 +11,10 @@
   (add-hook 'beancount-mode-hook #'cat-disable-electric-indent-chars)
   :custom
   (beancount-use-ido nil)
+  :mode-hydra
+  (beancount-mode
+   ("LSP"
+    (("e" eglot-hydra/body "eglot"))))
   :config
   (define-key beancount-mode-map (kbd "C-c C-n") #'outline-next-visible-heading)
   (define-key beancount-mode-map (kbd "C-c C-p") #'outline-previous-visible-heading)
@@ -17,3 +23,6 @@
     "Open fava url as soon as the address is announced."
     (if-let ((url-index (string-match "\\(http://.+:[0-9]+\\)" output)))
         (browse-url (match-string 1 output)))))
+
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs '(beancount-mode . ("beancount-language-server" "--stdio"))))
