ac4ca47e43c669e6105ba8d8e3497eefa31474b5
Misaka <chuxubank@qq.com>
2024-10-30 21:13:59 +0800

    Improve org-noter-citar

--- cats/+org-notes.el
+++ cats/+org-notes.el
@@ -16,38 +16,46 @@
   ;;  (if (string= (getenv "HOST_TYPE") "work")
   ;;      'other-frame
   ;;    'horizontal-split))
-  ;; (org-noter-use-indirect-buffer nil)
   (org-noter-doc-split-fraction '(0.6 . 0.5))
   :config
   (setq org-noter--inhibit-location-change-handler t)
-  (defun +org-noter-update-page-info (&optional arg)
-    "Update the (precise) page info of the current note."
-    (interactive "P")
-    (org-entry-put nil org-noter-property-note-location
-                   (org-noter--pretty-print-location
-                    (org-noter--doc-approx-location
-                     (when arg (org-noter--get-precise-info))))))
   (define-key org-noter-notes-mode-map (kbd "M-i") #'+org-noter-update-page-info)
+  (add-to-list 'org-noter-parse-document-property-hook #'org-noter-orb-citar-find-document-from-refs)
+  (add-to-list 'org-noter-find-additional-notes-functions #'org-noter-citar-find-key-from-this-file))
 
-  (with-eval-after-load 'org-roam
-    (org-noter-enable-org-roam-integration))
+(defun +org-noter-update-page-info (&optional arg)
+  "Update the (precise) page info of the current note."
+  (interactive "P")
+  (org-entry-put nil org-noter-property-note-location
+                 (org-noter--pretty-print-location
+                  (org-noter--doc-approx-location
+                   (when arg (org-noter--get-precise-info))))))
 
-  (defun org-noter-orb-citar-find-document-from-refs (&optional cite-key)
-    "Return a note file associated with CITE-KEY.
+(defun org-noter-orb-citar-find-document-from-refs (&optional cite-key)
+  "Return a note file associated with CITE-KEY.
 When there is more than one note files associated with CITE-KEY, have
 user select one of them."
-    (let* ((key (or cite-key (orb-get-node-citekey)))
-           (file-table (citar-get-files key))
-           (files '()))
-      (when file-table
-        (maphash (lambda (key file-list)
-                   (setq files (append files file-list)))
-                 file-table))
-      (cond ((= (length files) 1)
-             (car files))
-            ((> (length files) 1)
-             (completing-read (format "Which document from %s?: " key) files)))))
-  (add-to-list 'org-noter-parse-document-property-hook #'org-noter-orb-citar-find-document-from-refs))
+  (let* ((key (or cite-key (orb-get-node-citekey)))
+         (file-table (citar-get-files key))
+         (files '()))
+    (when file-table
+      (maphash (lambda (key file-list)
+                 (setq files (append files file-list)))
+               file-table))
+    (cond ((= (length files) 1)
+           (car files))
+          ((> (length files) 1)
+           (completing-read (format "Which document from %s?: " key) files)))))
+
+(defun org-noter-citar-find-key-from-this-file (filename)
+  (let (entry-alist)
+    (maphash (lambda (key entry)
+               (when-let ((file (citar-get-value citar-file-variable entry)))
+                 (push (cons file key) entry-alist)))
+             (citar-get-entries))
+    (let ((key (alist-get filename entry-alist nil nil (lambda (s regexp)
+                                                         (string-match-p regexp s)))))
+      (orb-find-note-file key))))
 
 (use-package deft
   :custom
