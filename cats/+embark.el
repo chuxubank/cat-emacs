ae87858ea2e3eb5c3e0033125005ef1077fa31d6
Misaka <chuxubank@qq.com>
2025-01-02 12:23:11 +0800

    feat: use embark only when which-key not displayed

--- cats/+embark.el
+++ cats/+embark.el
@@ -65,7 +65,6 @@
           ("C-\"" . avy-embark-collect-act))))
 
 (when (package-installed-p 'which-key)
-  (setq which-key--prefix-help-cmd-backup #'embark-prefix-help-command)
   (defun embark-which-key-indicator ()
     "An embark indicator that displays keymaps using which-key.
 The which-key help message will show the type and value of the
@@ -78,9 +77,9 @@ targets."
          (if (eq (plist-get (car targets) :type) 'embark-become)
              "Become"
            (format "Act on %s '%s'%s"
-                   (plist-get (car targets) :type)
-                   (embark--truncate-target (plist-get (car targets) :target))
-                   (if (cdr targets) "…" "")))
+                     (plist-get (car targets) :type)
+                     (embark--truncate-target (plist-get (car targets) :target))
+                     (if (cdr targets) "…" "")))
          (if prefix
              (pcase (lookup-key keymap prefix 'accept-default)
                ((and (pred keymapp) km) km)
--- cats/+which-key.el
+++ cats/+which-key.el
@@ -3,5 +3,4 @@
 (use-package which-key
   :hook (after-init . which-key-mode)
   :custom
-  (which-key-use-C-h-commands nil)
   (which-key-lighter nil))
