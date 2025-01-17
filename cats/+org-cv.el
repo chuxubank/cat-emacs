4fc45722b3627a9ac8299531cd50ed3344f6e531
Misaka <chuxubank@qq.com>
2025-01-17 14:26:56 +0800

    chore(org-cv): use origin url since pr merged
    https://gitlab.com/Titan-C/org-cv/-/merge_requests/13

--- cats/+org-cv.el
+++ cats/+org-cv.el
@@ -2,21 +2,21 @@
 
 (use-package ox-altacv
   :vc (ox-altacv
-       :url "https://gitlab.com/chuxubank/org-cv"
+       :url "https://gitlab.com/Titan-C/org-cv"
        :rev :newest)
   :demand t
   :after org)
 
 (use-package ox-moderncv
   :vc (ox-moderncv
-       :url "https://gitlab.com/chuxubank/org-cv"
+       :url "https://gitlab.com/Titan-C/org-cv"
        :rev :newest)
   :demand t
   :after org)
 
 (use-package ox-awesomecv
   :vc (ox-awesomecv
-       :url "https://gitlab.com/chuxubank/org-cv"
+       :url "https://gitlab.com/Titan-C/org-cv"
        :rev :newest)
   :demand t
   :after org)
