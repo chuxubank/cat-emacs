f7272709135de93d2e30fd758e2e16e9b8dc8e33
Misaka <chuxubank@qq.com>
2025-03-05 12:29:01 +0800

    chore(js): remove json-mode

--- cats/+js.el
+++ cats/+js.el
@@ -4,15 +4,9 @@
   :ensure nil
   :ensure-system-package
   (typescript-language-server . "bun add -g typescript-language-server")
+  (vscode-json-languageserver . "bun add -g vscode-json-languageserver")
   :custom
   (js-indent-level 2))
-
-(use-package json-mode
-  :ensure-system-package
-  (vscode-json-languageserver . "bun add -g vscode-json-languageserver")
-  :bind
-  (:map json-mode-map
-        ("C-c C-w" . json-mode-kill-path)))
 
 (defun cat-is-node-package-lock-buffer ()
   (string-match-p "package-lock.json" (buffer-name)))
