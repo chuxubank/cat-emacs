3417f9c875ec479cbbdb0035f3a022e1190a7301
Misaka <chuxubank@qq.com>
2025-08-07 10:11:45 +0800

    feat: use telega-transient and telega-mnz
    Use `telega-transient` and `telega-mnz` for interacting with telega.

--- cats/+im.el
+++ cats/+im.el
@@ -4,9 +4,12 @@
   (:color teal :title (+with-icon "nf-md-chat" "Instant Messaging"))
   ("" ()))
 
+(defvar cat-telega-contrib-load-path
+  (expand-file-name "contrib" (file-name-directory (locate-library "telega"))))
+
 (use-package telega
   :hook
-  (telega-mode . telega-mode-line-mode)
+  (telega-load . telega-mode-line-mode)
   :bind
   (:map telega-prefix-map
         ("k" . #'telega-kill))
@@ -15,7 +18,6 @@
   (:map telega-msg-button-map
         ("SPC" . nil))
   :init
-  (defalias 'telega-prefix telega-prefix-map)
   (when IS-WSL
     (setq telega-docker-run-command "docker run --security-opt apparmor=unconfined -i -u %u -v %w:%w -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:$XAUTHORITY -v /var/run/dbus:/var/run/dbus -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY --net=host %i"))
   :custom
@@ -29,14 +31,21 @@
   (telega-video-player-command "mpv")
   (telega-chat-input-markups '("markdown2" nil))
   (telega-open-file-function #'org-open-file)
-  (telega-completing-read-function completing-read-function)
+  (telega-completing-read-function completing-read-function))
+
+(use-package telega-transient
+  :load-path cat-telega-contrib-load-path
+  :commands #'telega-transient-telega
   :pretty-hydra
   (cat-im
    ("Telegram"
-    (("t" #'telega-prefix "telega"))))
+    (("t" #'telega-transient-telega "telega"))))
   :config
-  (require 'telega-mnz)
-  (global-telega-mnz-mode))
+  (telega-transient-mode))
+
+(use-package telega-mnz
+  :load-path cat-telega-contrib-load-path
+  :hook (telega-load . global-telega-mnz-mode))
 
 (use-package ement
   :init
