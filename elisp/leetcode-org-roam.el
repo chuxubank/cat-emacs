b269004ffda5bab719c03bf25b32a3d9130d5cfe
Misaka <chuxubank@qq.com>
2025-09-23 14:40:37 +0800

    docs: update leetcode-org-roam docs

--- elisp/leetcode-org-roam.el
+++ elisp/leetcode-org-roam.el
@@ -1,16 +1,67 @@
-;; -*- lexical-binding: t; -*-
+;;; leetcode-org-roam.el --- Capture LeetCode problems into Org-roam -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Misaka
+
+;; Author: Misaka <chuxubank@qq.com>
+;; Maintainer: Misaka <chuxubank@qq.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1") (leetcode "0.2") (org-roam "2.0") (aio "1.0"))
+;; Keywords: tools, outlines, leetcode, org-roam
+;; Homepage: https://github.com/chuxubank/leetcode-org-roam.el
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; This package integrates `leetcode.el' with `org-roam', making it
+;; possible to capture LeetCode problems directly into your Org-roam
+;; knowledge base.
+;;
+;; Features:
+;;
+;; - Fetch LeetCode problem metadata (title, slug, difficulty, tags, etc.)
+;; - Convert problem content (HTML) to Org format using Pandoc
+;; - Capture problem into Org-roam with :PROPERTIES:
+;; - Insert code template snippet based on `leetcode-prefer-language'
+;;
+;; Usage:
+;;
+;;   (require 'leetcode-org-roam)
+;;   (leetcode)
+;;
+;;   ;; Then run interactively:
+;;   M-x leetcode-org-roam-capture
+;;
+;; You will be prompted for the LeetCode problem ID, and the
+;; corresponding problem will be inserted into Org-roam using your
+;; `org-roam-capture-ref-templates`.
+
+;;; Code:
 
 (require 'leetcode)
 (require 'org-roam-capture)
+(require 'aio)
+
+(defgroup leetcode-org-roam nil
+  "Capture LeetCode problems into Org-roam."
+  :prefix "leetcode-org-roam-"
+  :group 'tools
+  :link '(url-link :tag "GitHub" "https://github.com/chuxubank/leetcode-org-roam"))
+
+(defcustom leetcode-org-roam-pandoc-command "pandoc"
+  "Pandoc command used to convert HTML problem content to Org."
+  :type 'string
+  :group 'leetcode-org-roam)
 
 (defun leetcode-org-roam--html-to-org (html)
-  "Convert LeetCode HTML problem content to org format using pandoc."
+  "Convert LeetCode HTML problem content to Org format using Pandoc.
+Requires `pandoc` to be installed and available in PATH."
   (let* ((infile  (make-temp-file "leetcode-html-" nil ".html"))
          (outfile (make-temp-file "leetcode-org-" nil ".org")))
     (with-temp-file infile
       (insert html))
     (with-temp-buffer
-      (unless (zerop (call-process "pandoc" nil t nil
+      (unless (zerop (call-process leetcode-org-roam-pandoc-command nil t nil
                                    "-f" "html" "-t" "org"
                                    "-o" outfile infile))
         (error "Pandoc failed: %s" (buffer-string))))
@@ -18,8 +69,11 @@
       (insert-file-contents outfile)
       (buffer-string))))
 
+;;;###autoload
 (aio-defun leetcode-org-roam-capture (id)
-  "Capture a LeetCode problem into org-roam by problem ID."
+  "Capture a LeetCode problem into Org-roam by problem ID.
+Fetches metadata, problem statement, and code snippet using
+`leetcode', and creates a new Org-roam node."
   (interactive (list (read-string "Org roam capture leetcode problem by problem id: "
                                   (when (derived-mode-p 'leetcode--problems-mode)
                                     (leetcode--get-current-problem-id)))))
@@ -32,7 +86,7 @@
          (title-slug (leetcode--slugify-title title))
          (difficulty (leetcode-problem-difficulty problem))
          (tags (string-join (leetcode-problem-tags problem) ":"))
-         (content (leetcode--html-to-org (leetcode-problem-content problem-with-content)))
+         (content (leetcode-org-roam--html-to-org (leetcode-problem-content problem-with-content)))
          (snippets (leetcode-problem-snippets problem-with-snippets))
          (snippet (progn
                     (leetcode--set-lang snippets)
@@ -56,3 +110,7 @@
      :node (org-roam-node-create :title title)
      :props data
      :templates org-roam-capture-ref-templates)))
+
+(provide 'leetcode-org-roam)
+
+;;; leetcode-org-roam.el ends here
