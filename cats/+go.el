044446e40be750eafc799ea8de18f8c84a3dbf2f
Misaka <chuxubank@qq.com>
2025-10-14 22:59:45 +0800

    feat(go): add configuration for go-mode and ob-go

--- cats/+go.el
+++ cats/+go.el
@@ -1,5 +1,13 @@
 ;; -*- lexical-binding: t; -*-
 
+(use-package go-mode)
+
+(use-package ob-go
+  :demand
+  :after org
+  :config
+  (add-to-list 'org-babel-load-languages '(go . t)))
+
 (use-package templ-ts-mode)
 
 (use-package go-template-mode
