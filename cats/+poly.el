ca80bc0ec193a2aad564f55b40604ec0b760f139
Misaka <chuxubank@qq.com>
2025-10-29 22:57:45 +0800

    chore(config): associate .plist files with nxml-mode

--- cats/+poly.el
+++ cats/+poly.el
@@ -3,6 +3,8 @@
 (use-package polymode
   :delight (polymode-minor-mode " "))
 
+(add-to-list 'auto-mode-alist '("\\.plist\\'" . nxml-mode))
+
 (use-package poly-any-jinja2
   :demand t
   :ensure nil)
