651eec134de1e0a3ff7e6f5e2845a173d6f7a2d1
Misaka <chuxubank@qq.com>
2026-03-20 18:10:56 +0800

    feat(yaml): add keybindings for yaml-pro subtree navigation

--- cats/+yaml.el
+++ cats/+yaml.el
@@ -4,4 +4,10 @@
   :mode "\\.ya?ml\\'")
 
 (use-package yaml-pro
-  :hook (yaml-ts-mode . yaml-pro-ts-mode))
+  :hook (yaml-ts-mode . yaml-pro-ts-mode)
+  :bind
+  (:map yaml-pro-ts-mode-map
+        ("M-<up>" . #'yaml-pro-ts-move-subtree-up)
+        ("M-<down>" . #'yaml-pro-ts-move-subtree-down)
+        ("M-<left>" . #'yaml-pro-ts-unindent-subtree)
+        ("M-<right>" . #'yaml-pro-ts-indent-subtree)))
