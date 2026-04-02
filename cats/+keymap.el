7400f09afaf478a7fd56a5bce98dd5e11020d029
Misaka <chuxubank@qq.com>
2026-04-02 13:44:58 +0800

    refactor(keymap): move agent, term, and vibe bindings to plugin prefix
    Move `cat-agent/body`, `cat-term/body`, and `cat-vibe/body` from
    `cat-map` to `cat-plugin-prefix` for more consistent key organization.

--- cats/+keymap.el
+++ cats/+keymap.el
@@ -31,6 +31,7 @@
   :doc "Keymap for plugins."
   :name "Cat Plugin"
   :prefix 'cat-plugin-prefix
+  "a" #'cat-agent/body
   "b" #'cat-blog/body
   "c" 'chezmoi-prefix
   "d" 'cat-dev-doc-prefix
@@ -48,13 +49,14 @@
   "P" #'password-store-menu
   "r" #'elfeed
   "R" #'magit-list-repositories
+  "t" #'cat-term/body
+  "v" #'cat-vibe/body
   "w" #'webjump)
 
 (defvar-keymap cat-map
   :keymap mode-specific-map
   ":" #'avy-goto-char-timer
   "a" #'org-agenda
-  "A" #'cat-agent/body
   "c" #'org-capture
   "d" 'cat-org-roam-dailies-prefix
   "e" #'cat-eudc/body
@@ -69,8 +71,6 @@
   "r" 'cat-org-roam-prefix
   "s" #'rg-menu
   "t" 'cat-toggle-prefix
-  "T" #'cat-term/body
-  "v" #'cat-vibe/body
   "w" #'cat-workspace/body)
 
 (defvar-keymap cat-escape-map
