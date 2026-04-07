d01e68a44d388445c3784fe8a65dc3801e4e3bee
Misaka <chuxubank@qq.com>
2026-04-07 11:09:02 +0800

    fix(cats): update anthropic model from claude-opus to claude-sonnet

--- cats/+vibe.el
+++ cats/+vibe.el
@@ -84,7 +84,7 @@
                           gptel--iv
                         gptel--ollama)
         gptel-model (if (eq HOST_ENV 'iv)
-                        'global.anthropic.claude-opus-4-6-v1
+                        'global.anthropic.claude-sonnet-4-6
                       'gemma4)))
 
 (use-package gptel-magit
@@ -96,7 +96,7 @@
                                 gptel--iv
                               gptel--ollama)
         gptel-magit-model (if (eq HOST_ENV 'iv)
-                              'global.anthropic.claude-opus-4-6-v1
+                              'global.anthropic.claude-sonnet-4-6
                             'gemma4))
   (defun gptel-magit--generate (callback)
     "Generate a commit message for current magit repo.
