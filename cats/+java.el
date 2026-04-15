76b4e216bab460c4312d666904414be7e4a54f80
Misaka <chuxubank@qq.com>
2026-04-15 09:14:53 +0800

    refactor(config): modularize language package definitions
    Splits JVM configuration into a dedicated `java.el` file and adds a new dedicated `kotlin.el` file to handle Kotlin mode and linter setup, improving code structure and maintainability.

--- cats/+java.el
+++ cats/+java.el
@@ -1,28 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package kotlin-mode
-  :unless EMACS29+
-  :custom
-  (kotlin-args-repl '("-Xrepl")))
-
-(use-package kotlin-ts-mode
-  :when EMACS29+
-  :mode-hydra
-  (("Test"
-    (("t" kotlin-ts-mode-goto-test-file "go to test file")
-     ("r" kotlin-ts-mode-run-current-test-function "run current test function")
-     ("R" kotlin-ts-mode-run-current-test-class "run current test class")))))
-
-(use-package ob-kotlin
-  :vc (:url "https://github.com/chuxubank/ob-kotlin")
-  :demand
-  :after org
-  :config
-  (add-to-list 'org-babel-load-languages '(kotlin . t)))
-
-(use-package flycheck-kotlin
-  :hook (flycheck-mode . flycheck-kotlin-setup))
-
 (use-package java-imports
   :disabled
   :hook ((java-mode kotlin-mode) . java-imports-scan-file)
