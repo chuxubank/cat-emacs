b56f15043f090bf000c9d3f513b49301452d3069
Misaka <chuxubank@qq.com>
2026-07-07 15:12:30 +0800

    refactor: move package-vc upgrade skip logic to core module

--- modules/base/+package.el
+++ modules/base/+package.el
@@ -55,36 +55,3 @@
                                (url-host (url-generic-parse-url url))
                                (if (eq symbol-status 'success)
                                    (request-response-header response "Last-Modified"))))))))))))
-
-(defun cat/package-vc-skip-if-same-hash (orig-fn pkg-desc)
-  "Skip `package-vc-upgrade' if PKG-DESC is already at the latest commit.
-Only applies to Git VC packages; otherwise, run ORIG-FN.
-
-This version safely handles branches without upstream and can optionally fetch."
-  (let* ((pkg-dir (package-desc-dir pkg-desc))
-         (backend (vc-responsible-backend pkg-dir)))
-    (if (not (eq backend 'Git))
-        (funcall orig-fn pkg-desc)
-      (let ((default-directory pkg-dir))
-        (condition-case err
-            (let* ((upstream (string-trim
-                              (with-output-to-string
-                                (vc-git-command standard-output nil nil
-                                                "rev-parse" "--abbrev-ref" "@{u}")))))
-              (if (string-empty-p upstream)
-                  (funcall orig-fn pkg-desc)
-                (vc-git-command t 0 nil "fetch" "--quiet")
-                (let* ((local (string-trim
-                               (with-output-to-string
-                                 (vc-git-command standard-output nil nil "rev-parse" "HEAD"))))
-                       (remote (string-trim
-                                (with-output-to-string
-                                  (vc-git-command standard-output nil nil "rev-parse" "@{u}")))))
-                  (if (string= local remote)
-                      (message "Package %s already up-to-date" (package-desc-name pkg-desc))
-                    (funcall orig-fn pkg-desc)))))
-          (error
-           (funcall orig-fn pkg-desc)))))))
-
-
-(advice-add 'package-vc-upgrade :around #'cat/package-vc-skip-if-same-hash)
