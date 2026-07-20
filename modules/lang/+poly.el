19b53fb71b1191bef4fe9774c25f3e594ae5f450
Misaka <chuxubank@qq.com>
2026-07-20 12:06:04 +0800

    feat(poly): add custom lighters for template modes

--- modules/lang/+poly.el
+++ modules/lang/+poly.el
@@ -7,8 +7,12 @@
 
 (use-package poly-any-jinja2
   :vc (:url "https://github.com/chuxubank/poly-any-template"
-            :main-file "poly-any-jinja2.el"))
+            :main-file "poly-any-jinja2.el")
+  :custom
+  (poly-any-jinja2-lighter "  "))
 
 (use-package poly-any-go-template
   :vc (:url "https://github.com/chuxubank/poly-any-template"
-            :main-file "poly-any-go-template.el"))
+            :main-file "poly-any-go-template.el")
+  :custom
+  (poly-any-go-template-lighter "  "))
