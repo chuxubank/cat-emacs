bcfb8af2a1790bb6935bd9c39f4830a60742e988
Misaka <chuxubank@qq.com>
2026-07-31 01:27:21 +0800

    fix(chezmoi): preserve shell host from shebang

--- modules/apps/+chezmoi.el
+++ modules/apps/+chezmoi.el
@@ -5,6 +5,14 @@
   (unless (bound-and-true-p polymode-mode)
     (poly-any-go-template-mode)))
 
+(defun cat/chezmoi-template-shell-host-filename (filename)
+  "Preserve a shell host selected from the shebang for FILENAME."
+  (if (and filename
+           (derived-mode-p 'sh-mode)
+           (not (file-name-extension filename)))
+      (concat filename ".sh")
+    filename))
+
 (use-package chezmoi-mode
   :vc (:url "https://github.com/chuxubank/chezmoi-mode")
   :delight " "
@@ -16,7 +24,8 @@
   (poly-any-go-template-extra-file-name-rules
    '(chezmoi-template-source-file-p))
   (poly-any-template-host-filename-functions
-   '(chezmoi-template-normalize-host-filename)))
+   '(chezmoi-template-normalize-host-filename
+     cat/chezmoi-template-shell-host-filename)))
 
 (defun cat/chezmoi-mode-p ()
   "Return non-nil if `chezmoi-mode' minor mode is enabled in the current buffer."
