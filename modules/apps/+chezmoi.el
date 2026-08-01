9bdfb62e827551a1f9368e1786b51202be050964
Misaka <chuxubank@qq.com>
2026-08-01 20:04:33 +0800

    fix(chezmoi): use add-to-list to avoid overwriting template config

--- modules/apps/+chezmoi.el
+++ modules/apps/+chezmoi.el
@@ -6,11 +6,12 @@
   :hook
   (chezmoi-template-mode . poly-any-go-template-mode)
   :config
-  (require 'poly-any-go-template)
-  (setq poly-any-go-template-extra-file-name-rules
-        '(chezmoi-template-source-file-p)
-        poly-any-template-host-filename-functions
-        '(chezmoi-template-normalize-host-filename)))
+  (add-to-list
+   'poly-any-go-template-extra-file-name-rules
+   #'chezmoi-template-source-file-p)
+  (add-to-list
+   'poly-any-template-host-filename-functions
+   #'chezmoi-template-normalize-host-filename))
 
 (defun cat/chezmoi-mode-p ()
   "Return non-nil if `chezmoi-mode' minor mode is enabled in the current buffer."
