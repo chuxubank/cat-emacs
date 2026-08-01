4dd993a2a6e42d963eb71ad57b54aec3bd7371d7
Misaka <chuxubank@qq.com>
2026-08-01 19:44:04 +0800

    fix(chezmoi): use idempotent template mode activation

--- modules/apps/+chezmoi.el
+++ modules/apps/+chezmoi.el
@@ -1,22 +1,16 @@
 ;; -*- lexical-binding: t; -*-
 
-(defun cat/chezmoi-template-mode-setup ()
-  "Select a pure or host-composed mode for a Chezmoi template."
-  (unless (bound-and-true-p polymode-mode)
-    (poly-any-go-template-mode)))
-
 (use-package chezmoi-mode
   :vc (:url "https://github.com/chuxubank/chezmoi-mode")
   :delight " "
   :hook
-  (chezmoi-template-mode . cat/chezmoi-template-mode-setup))
-
-(use-package poly-any-go-template
-  :custom
-  (poly-any-go-template-extra-file-name-rules
-   '(chezmoi-template-source-file-p))
-  (poly-any-template-host-filename-functions
-   '(chezmoi-template-normalize-host-filename)))
+  (chezmoi-template-mode . poly-any-go-template-mode)
+  :config
+  (require 'poly-any-go-template)
+  (setq poly-any-go-template-extra-file-name-rules
+        '(chezmoi-template-source-file-p)
+        poly-any-template-host-filename-functions
+        '(chezmoi-template-normalize-host-filename)))
 
 (defun cat/chezmoi-mode-p ()
   "Return non-nil if `chezmoi-mode' minor mode is enabled in the current buffer."
